236dec8b700e281cef5d89744ce587cb5fb3cbfd
Ivo Hedtke <ivo.hedtke@dbschenker.com>
2023-10-11 09:35:14 +0200

    Upgrade to SCIP 8.0.4

--- conanfile.py
+++ conanfile.py
@@ -75,9 +75,9 @@ class ScipPlusPlus(ConanFile):
         cmake_layout(self)
 
     def requirements(self):
-        self.requires("scip/8.0.3", transitive_headers=True)
+        self.requires("scip/8.0.4", transitive_headers=True)
         if self.options.with_tests:
-            self.requires("boost/1.81.0")  # required only for tests
+            self.requires("boost/[>=1.81.0 <2]")  # required only for tests
 
     def generate(self):
         tc = CMakeToolchain(self)
