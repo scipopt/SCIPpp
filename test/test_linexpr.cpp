392ca742b840b3ea4a44748c22a332e0c73282c2
Ivo Hedtke <ivo.hedtke@dbschenker.com>
2025-01-27 13:50:27 +0100

    Increase coverage

--- test/test_linexpr.cpp
+++ test/test_linexpr.cpp
@@ -55,9 +55,11 @@ BOOST_AUTO_TEST_CASE(AddVector)
     array coeff { 1, 1 };
     const auto A_VARS { model.addVars<2>("x_", coeff) };
     const vector<Var> VARS { A_VARS.begin(), A_VARS.end() };
-    LinExpr l;
-    l += VARS;
-    model.addConstr(l <= 1, "capacity");
+    LinExpr l1;
+    l1 += VARS;
+    model.addConstr(l1 <= 1, "capacity1");
+    LinExpr l2(VARS, vector { 2.0, 2.0 });
+    model.addConstr(l2 <= 2, "capacity2"); // duplicate, but different c'tor
     model.setObjsense(Sense::MAXIMIZE);
     model.solve();
     BOOST_TEST(model.getSolvingStatistic(statistics::PRIMALBOUND) == 1);
